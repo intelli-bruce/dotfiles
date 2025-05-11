47295403a569e9a494857d349130f79db8044842
bruce <bruce@intellieffect.com>
2025-05-11 09:15:26 +0900

    init.lua 수정

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,11 @@ vim.opt.expandtab = true
 vim.opt.mouse = "a"
 vim.opt.clipboard = "unnamedplus"
 
--- lazy.nvim 플러그인 매니저
+-- netrw 비활성화 (nvim-tree와 충돌 방지)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- lazy.nvim 플러그인 매니저 설정
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -20,6 +24,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- 플러그인 설치 목록
 require("lazy").setup({
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
@@ -35,7 +40,24 @@ require("lazy").setup({
   { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
   { "L3MON4D3/LuaSnip" },
+  { "tpope/vim-fugitive" },
 })
 
+-- 색상 테마 적용
 vim.cmd.colorscheme "catppuccin-mocha"
+
+-- nvim-tree 초기화
+require("nvim-tree").setup({})
+
+-- 상태바 초기화
+require("lualine").setup({})
+
+-- 단축키 설정
+vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true }) -- 탐색기 토글
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>", { noremap = true, silent = true }) -- 파일 검색
+
+-- 터미널 단축키
+vim.keymap.set("n", "<leader>t", ":split | terminal<CR>", { noremap = true, silent = true }) -- 가로 분할 터미널
+vim.keymap.set("n", "<leader>vt", ":vsplit | terminal<CR>", { noremap = true, silent = true }) -- 세로 분할 터미널
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true }) -- 터미널 모드 종료
 
