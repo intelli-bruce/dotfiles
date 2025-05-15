0235a18d2647283b6963ba739cced452683aa9ef
bruce <bruce@intellieffect.com>
2025-05-15 15:07:59 +0900

    wezterm 실행할때마다tmux 세션과 함께 실행되도록 수정

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,30 +3,32 @@ local act = wezterm.action
 
 
 return {
-   font = wezterm.font_with_fallback {
-    'JetBrainsMono Nerd Font',
-    'Apple SD Gothic Neo',
-    'D2Coding',
-  },
+    font = wezterm.font_with_fallback {
+        'JetBrainsMono Nerd Font',
+        'Apple SD Gothic Neo',
+        'D2Coding',
+    },
 
-  font_size = 16.0,
+    font_size = 16.0,
+
+    default_prog = { "/bin/zsh", "-c", "/opt/homebrew/bin/tmux attach -t main || /opt/homebrew/bin/tmux new -s main" },
 
 
-  color_scheme = "Dracula",
-  initial_rows = 30,
-  initial_cols = 100,
-  enable_tab_bar = false,
+    color_scheme = "Dracula",
+    initial_rows = 30,
+    initial_cols = 100,
+    enable_tab_bar = false,
 
-  window_background_opacity = 0.85,           -- 투명도
-  macos_window_background_blur = 20,          -- 아크릴 블러 (macOS 한정)
+    window_background_opacity = 0.85,  -- 투명도
+    macos_window_background_blur = 20, -- 아크릴 블러 (macOS 한정)
 
- -- 한글 입력 개선
-  use_ime = true,
-  enable_kitty_keyboard = true, -- 일부 조합 이슈 해결
+    -- 한글 입력 개선
+    use_ime = true,
+    enable_kitty_keyboard = true, -- 일부 조합 이슈 해결
 
-  -- macOS 한글 조합 지연 줄이기
-  macos_forward_to_ime_modifier_mask = "SHIFT|CTRL|ALT|CMD",
+    -- macOS 한글 조합 지연 줄이기
+    macos_forward_to_ime_modifier_mask = "SHIFT|CTRL|ALT|CMD",
 
-  -- true color 지원
-  enable_wayland = false,
+    -- true color 지원
+    enable_wayland = false,
 }
