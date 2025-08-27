fded8e6bf30b83ab922e80943156cff3d88f917f
brucechoe <bruce@intellieffect.com>
2025-08-28 07:23:55 +0900

    edit > nvim : cursor line 에서 relative 옵션 비활성화

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- 커서 라인 강조 활성화
+vim.opt.cursorline = true
+
+-- 줄 번호 표시
+vim.opt.number = true
+vim.opt.relativenumber = false
