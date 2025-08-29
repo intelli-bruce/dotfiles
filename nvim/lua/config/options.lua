54a31b4244f57127019641cc328fa4c96a6b31cc
Bruce <bruce@intellieffect.com>
2025-08-29 20:02:58 +0900

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- 스크롤 애니메이션 비활성화 (snacks.nvim)
+vim.g.snacks_animate = false
+
 -- 커서 라인 강조 활성화
 vim.opt.cursorline = true
 
